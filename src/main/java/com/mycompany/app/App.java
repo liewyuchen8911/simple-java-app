3ca41226213cf65d7bb2ae7effc496efca13a87d
Liew Yu Chen <liew.yu-chen@keysight.com>
2023-05-31 08:50:53 +0000

    webhooks

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("xxx");
+        System.out.println("last change for today");
     }
 
     public String getMessage() {
