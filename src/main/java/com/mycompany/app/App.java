9e3fc2486735f717875a809acfaddc8b73b54117
Liew Yu Chen <liew.yu-chen@keysight.com>
2023-05-31 08:23:27 +0000

    xxx

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("I want to sleep");
+        System.out.println("xxx");
     }
 
     public String getMessage() {
