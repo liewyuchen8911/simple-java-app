6a81f3f0118d191b5368c51b782473532fab03aa
Liew Yu Chen <liew.yu-chen@keysight.com>
2023-06-01 05:29:42 +0000

    pipeline with pollscm

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("last change for today");
+        System.out.println("okok");
     }
 
     public String getMessage() {
